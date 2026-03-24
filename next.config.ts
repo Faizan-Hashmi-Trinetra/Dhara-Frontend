140ffc92041e757c1365cbbd7422fca065b82fb2
Faizan Hashmi <mohd.faizan@trinetralabs.ai>
2026-03-24 22:31:28 +0530

    env name chnged

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: `${process.env.BACKEND_URL || 'https://dhara-rag-2.onrender.com'}/api/:path*`,
+        destination: `${process.env.NEXT_PUBLIC_BASE_URL || 'https://dhara-rag-2.onrender.com'}/api/:path*`,
       },
     ];
   },
